29a77d9b4d05e8bc61f90a4c96bf69f3e26e8cc2
Mathieu Fallows <mathieufallows@gmail.com>
2015-04-14 19:38:54 -0700

    form-based workflow is complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Blogger::Application.routes.draw do
-  resources :articles
+  root to: 'articles#index'
+
+  resources(:articles)
+  # get "/articles" => "articles#index"
+  # get "/articles/new" => "articles#new", as: :new_article
+  # get({"/articles/:id" => "articles#show", :as => :article})
+
+
 end
